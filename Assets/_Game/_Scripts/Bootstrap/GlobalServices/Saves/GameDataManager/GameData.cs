3154045fd29ddec840e3cf1901ec44f4964230e0
Teamofey-Izorsky <daniilgo471@gmail.com>
2026-06-24 19:12:24 +0300

    40 Lines record fix 2

--- Assets/_Game/_Scripts/Bootstrap/GlobalServices/Saves/GameDataManager/GameData.cs
+++ Assets/_Game/_Scripts/Bootstrap/GlobalServices/Saves/GameDataManager/GameData.cs
@@ -1,6 +1,20 @@
 public struct GameData
 {
     //Структура, хранящая данные рекордов и другую статистику, которую надо сохранить между сессиями
+    public GameData(float lines40Time = float.MaxValue)
+    {
+        BestStandartTime = 0;
+        BestStandartScore = 0;
+
+        Best40LinesTime = lines40Time;
+
+        BestBlirzScore = 0;
+        BestBlitzLinesCount = 0;
+
+        allTime = 0;
+        gamesPlayed = 0;
+    }
+
 
     //StandartMode
     public float BestStandartTime;
